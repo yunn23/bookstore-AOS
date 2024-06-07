6e17e3103b0199a4f70898c1ad5449af766bfcef
JeongSeoyun <xeonj@naver.com>
2024-06-07 21:02:27 +0900

    feat: MainActivity menu setOnClickListener 초기설정

--- src/main/java/com/example/finalhw/MainActivity.java
+++ src/main/java/com/example/finalhw/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.finalhw;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView coverImage;
     private boolean isCover01 = true;
+    private ImageButton menu01, menu02, menu03, menu04;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +33,45 @@ public class MainActivity extends AppCompatActivity {
                     coverImage.setImageResource(R.drawable.cover01);
                 }
                 isCover01 = !isCover01;
+
+            }
+        });
+
+        // 도서목록
+        menu01 = findViewById(R.id.menu01);
+        menu01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        // 로그인
+        menu02 = findViewById(R.id.menu02);
+        menu02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        // 고객센터
+        menu03 = findViewById(R.id.menu03);
+        menu03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        // 마이페이지
+        menu04 = findViewById(R.id.menu04);
+        menu04.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
             }
         });
