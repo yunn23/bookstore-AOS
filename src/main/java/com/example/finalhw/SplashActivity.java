4e50408e0a34293bc06ce8cc1d10c026724eb4c8
JeongSeoyun <xeonj@naver.com>
2024-06-08 18:04:12 +0900

    feat: Splash 기능 추가

---
+++ src/main/java/com/example/finalhw/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.finalhw;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int splashDelay = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, splashDelay);
+
+    }
+}
