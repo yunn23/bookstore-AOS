e51f981a8de235b5e243e5c58afa5fa5f58d7d8e
JeongSeoyun <xeonj@naver.com>
2024-06-08 15:34:27 +0900

    feat: actionbar 구현

--- src/main/java/com/example/finalhw/BooklistActivity.java
+++ src/main/java/com/example/finalhw/BooklistActivity.java
@@ -1,6 +1,11 @@
 package com.example.finalhw;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +15,36 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class BooklistActivity extends AppCompatActivity {
 
+    View view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booklist);
 
+        view = findViewById(R.id.layout);
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if ( id == R.id.home) {
+            return true;
+        } else if (id == R.id.booklist) {
+            return true;
+        } else if (id == R.id.cart) {
+            return true;
+        } return super.onOptionsItemSelected(item);
     }
 }
