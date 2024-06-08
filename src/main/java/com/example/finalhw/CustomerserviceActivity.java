14b8af3266ce0597b3cba99a48e9adbaa492c2db
JeongSeoyun <xeonj@naver.com>
2024-06-08 19:18:08 +0900

    feat: 고객센터 UI 구현

---
+++ src/main/java/com/example/finalhw/CustomerserviceActivity.java
@@ -0,0 +1,19 @@
+package com.example.finalhw;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CustomerserviceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customerservice);
+
+    }
+}
