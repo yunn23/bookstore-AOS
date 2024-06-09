0696392c6a374dc602590b6c681be71e2f751c88
JeongSeoyun <xeonj@naver.com>
2024-06-09 16:17:09 +0900

    feat: 마이페이지 버튼 클릭시 토스트 메세지 구현

--- src/main/java/com/example/finalhw/MypageActivity.java
+++ src/main/java/com/example/finalhw/MypageActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,6 +39,39 @@ public class MypageActivity extends AppCompatActivity {
             TextView username = findViewById(R.id.id);
             username.setText(id);
         }
+
+
+        ImageButton mymenu01 = findViewById(R.id.mymenu01);
+        mymenu01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "주문/결제 메뉴를 클릭했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ImageButton mymenu02 = findViewById(R.id.mymenu02);
+        mymenu02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "배송 메뉴를 클릭했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ImageButton mymenu03 = findViewById(R.id.mymenu03);
+        mymenu03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "취소/교환/반품 메뉴를 클릭했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ImageButton mymenu04 = findViewById(R.id.mymenu04);
+        mymenu04.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "회원계정 메뉴를 클릭했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         Button recentButton = findViewById(R.id.recentButton);
         recentButton.setOnClickListener(new View.OnClickListener() {
